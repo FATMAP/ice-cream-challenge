1ef07528e571a303e5b05345437eb895287b4528
Daniel Fernandez Rico <daniel.fdez@pm.me>
2022-05-28 18:05:55 +0200

    Added a basic search with Fuse

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,48 @@
-import './App.scss';
+import { useState, useEffect, useMemo } from "react";
+import Fuse from "fuse.js";
+import SearchIcon from "./assets/search.svg";
+import CloseIcon from "./assets/close.svg";
+import "./App.scss";
+
+type DBKeys = "name" | "type";
+type Shop = Record<DBKeys, string>;
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [shops, setShops] = useState<Shop[]>([]);
+  const [searchResults, setResults] = useState<Shop[]>([]);
+  const isSearching = inputText.length > 0;
+
+  const fuse = useMemo(() => new Fuse(shops, {
+    keys: ["name"],
+  }), [shops]);
+
+  useEffect(() => {
+    fetch("http://127.0.01:8000/shops")
+      .then((r) => r.json())
+      .then((r) => setShops(r));
+  }, []);
+
+  useEffect(() => {
+    const results = fuse
+      .search(inputText || "", { limit: 10 })
+      .map((o: { item: Shop }) => o.item);
+    setResults(results);
+  }, [inputText, fuse]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <img src={SearchIcon} alt="Search icon" />
+      <input
+        placeholder="Search"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      ></input>
+      {isSearching && <img src={CloseIcon} alt="Search icon" />}
+      <ul>
+        {isSearching && searchResults.map((item: Shop) => <li>{item.name}</li>)}
+      </ul>
+    </main>
   );
 }
 
